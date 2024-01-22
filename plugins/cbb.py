69c7f8e5c75405b91fe447ff1a7fbb9c529f25ed
RaakRaghu <123383385+RaakRaghu@users.noreply.github.com>
2024-01-22 08:08:02 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/KWICBOTS/File-sharing-Bot'>Click here</a>\n○ Channel : @kwicbotupdates\n○ Support Group : @kwicbotupdates</b>",
+            text = f"<b> Join Updates Channel🔥 : <a href='https://t.me/sunnyislub_x'>Click here</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
