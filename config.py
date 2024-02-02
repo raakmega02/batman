98d296f88c01e9ac389c7eac9b8ee73498266dc5
RaakRaghu <123383385+RaakRaghu@users.noreply.github.com>
2024-02-02 19:08:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,8 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001979976612"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\n Join For More Links 1. https://t.me/+kxI_UMH4ZxljODg1 2. https://t.me/+wxDSKyTwAMowZGFl")
-try:
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\n <b>○ Direct Mega Links : <a href='https://t.me/+QPyHEA1xCVtlNTcx'>CLICK HERE</a>\n○ Daily Special Mega Links: <a href='https://t.me/+SNSFScToH4piNTk1'>CLICK HERE</a>\n○ TeraBox unlimited Links : <a href='https://t.me/+S7SN-3vnD1s4YjEx'>CLICK HERE</a>\n○ Special Leak Updates : <a href='https://t.me/+wxDSKyTwAMowZGFl'>CLICK HERE</a>\n○ Main Channel : <a href='https://t.me/+kxI_UMH4ZxljODg1'>CLICK HERE </a>\n</b> Join All And Enjoy")
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
         ADMINS.append(int(x))
